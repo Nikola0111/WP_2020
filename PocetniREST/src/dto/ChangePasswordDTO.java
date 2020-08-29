1ee5cd8345667d57e16fef02cb26efdab0ded497
PopovicV <vladimirpopovic@live.com>
2020-08-29 20:46:11 +0200

    Changing password

---
+++ PocetniREST/src/dto/ChangePasswordDTO.java
@@ -0,0 +1,37 @@
+package dto;
+
+public class ChangePasswordDTO {
+
+	private String userName;
+	private String password;
+	private String newPassword;
+	
+	public ChangePasswordDTO() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String username) {
+		this.userName = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	
+}
--- PocetniREST/src/services/UserService.java
+++ PocetniREST/src/services/UserService.java
@@ -2,10 +2,18 @@ package services;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 import dao.UserDAO;
+import dto.ChangePasswordDTO;
+import model.User;
 
 @Path("User")
 public class UserService {
@@ -22,5 +30,28 @@ public class UserService {
 		if (context.getAttribute("users") == null) {
 			context.setAttribute("users", new UserDAO(context.getRealPath("")));
 		}
+	}
+	
+	@PUT
+	@Path("changePassword")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public boolean changePassword(ChangePasswordDTO userInfo, @Context HttpServletRequest request) {
+		
+		UserDAO users = getUsers();
+		User currentUser = users.find(userInfo.getUserName(), userInfo.getPassword());
+		if (currentUser != null) {
+			currentUser.setPassword(userInfo.getNewPassword());
+			users.saveUsers(context.getRealPath(""));
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public UserDAO getUsers() {
+		UserDAO users = (UserDAO) context.getAttribute("users");
+		return users;
 	}
 }

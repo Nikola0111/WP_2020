9bb382453f243b57fd85f2a19b0efa36cbaf0d07
PopovicV <vladimirpopovic@live.com>
2020-08-31 11:28:11 +0200

    Register and login

---
+++ PocetniREST/src/dto/ChangeUserDTO.java
@@ -0,0 +1,69 @@
+package dto;
+
+public class ChangeUserDTO {
+
+	private String previousUserName;
+	private String newUserName;
+	private String password;
+	private String name;
+	private String surname;
+	
+	public ChangeUserDTO() {
+		
+	}
+
+	public String getPreviousUserName() {
+		return previousUserName;
+	}
+
+
+
+	public void setPreviousUserName(String previousUserName) {
+		this.previousUserName = previousUserName;
+	}
+
+
+
+	public String getNewUserName() {
+		return newUserName;
+	}
+
+
+
+	public void setNewUserName(String newUserName) {
+		this.newUserName = newUserName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	
+	
+}
---
+++ PocetniREST/src/dto/RegisterDTO.java
@@ -0,0 +1,56 @@
+package dto;
+
+public class RegisterDTO {
+
+	private String userName;
+	private String password;
+	private String name;
+	private String surname;
+	private String userGender;
+	
+	public RegisterDTO() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getUserGender() {
+		return userGender;
+	}
+
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	
+	
+}

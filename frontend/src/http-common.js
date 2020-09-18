fe131f5fffbfb68e323c983084ac4deea294e7aa
PopovicV <vladimirpopovic@live.com>
2020-09-18 03:58:02 +0200

    Final commit

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: "/PocetniREST/rest",
+    baseURL: "/",
     headers: {
         "Content-type": "application/json",
     }
